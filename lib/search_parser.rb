e8378be857747d85e7a86d29cbf8086631172ef1
Bill Dueber <bill@dueber.com>
2023-07-14 23:04:18 -0400

    Empty shell

---
+++ lib/search_parser.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+
+require_relative "search_parser/version"
+require_relative "search_parser/parser"
+require_relative "search_parser/parser/config"
+
+module SearchParser
+
+  def self.parser(config)
+  end
+
+end
+
