6380f8e4fe8f2b50aa08c657b8e4d579c5ed0d80
Bill Dueber <bill@dueber.com>
2023-08-06 23:09:04 -0400

    Improve shake

--- lib/search_parser/node/fielded.rb
+++ lib/search_parser/node/fielded.rb
@@ -5,9 +5,17 @@ require "search_parser/node/generic"
 module SearchParser::Node
   class Fielded < Generic
     attr_accessor :fieldname
-    def initialize(fieldname, node)
-      super(node)
+    def initialize(fieldname, value)
+      super(value)
       @fieldname = fieldname
+    end
+
+    def dup(f: fieldname, n: node)
+      self.class.new(f, n.dup)
+    end
+
+    def shake
+      dup(f: fieldname, n: value.dup.shake)
     end
 
     def to_s
